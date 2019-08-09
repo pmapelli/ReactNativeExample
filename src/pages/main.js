1dba0bc4c822985eafcef4d0f4193158e2918f2f
pedro.mapelli <pedro.mapelli@TI-PC>
2019-08-09 17:48:26 -0300

    Inclui paginacao

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,19 +8,25 @@ export default class Main extends Component {
     };
 
     state = {
+        productInfo: {},
         docs: [],
+        page: 1,
     };
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get("/products");
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
-        const { docs } = response.data;
+        const { docs, ...productInfo } = response.data;
 
-        this.setState({ docs });
+        this.setState({ 
+            docs: [... this.state.docs, ...docs], 
+            productInfo,
+            page
+        });
     }
 
     renderItem = ({ item }) => (
@@ -28,11 +34,25 @@ export default class Main extends Component {
             <Text style={styles.productTitle}>{item.title}</Text>
             <Text style={styles.productDescription}>{item.description}</Text>
 
-            <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+            <TouchableOpacity 
+                style={styles.productButton} 
+                onPress={() => {
+                    this.props.navigation.navigate("Product");
+                }}>
                 <Text style={styles.productButtonText}>Acessar</Text>
             </TouchableOpacity>
         </View>
     )
+
+    loadMore = () => {
+        const { page, productInfo } = this.state;
+
+        if ( page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    };
 
     render() {
         return (
@@ -42,6 +62,8 @@ export default class Main extends Component {
                     data={this.state.docs}
                     keyExtractor={item => item._id}
                     renderItem={this.renderItem}
+                    onEndReached={this.loadMore}
+                    onEndReachedThreshold={0.1}
                 />
             </View>
         );
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { createAppContainer, createStackNavigator } from "react-navigation";
+
 import Main from "./pages/main";
+import Product from "./pages/product";
 
 export default createAppContainer(
     createStackNavigator({
-    Main
-}, {
+    Main,
+    Product
+}, 
+{
     navigationOptions: {
         headerStyle: {
             backgroundColor: "#DA552F"
